92e0a8fc7b6fae3a5bd631a9083cfaeb37efe862
Nadya Voynich <nadya.voynich@gmail.com>
2024-10-18 20:52:14 +0200

    refactor: Reduce the frequency of write requests to avoid hitting the quato limit

--- main.py
+++ main.py
@@ -11,23 +11,18 @@ client = gspread.authorize(credentials)
 # Open the Google Sheet
 sheet = client.open('GooglePlay Reviews').sheet1
 
-package_name = 'com.ichi2.anki'
-
 # Fetch reviews
+package_name = 'com.ichi2.anki'
 result, continuation_token = reviews(
     package_name,
     lang='en',
-    count=1000,  # Number of reviews per request
+    count=10000,  # Number of reviews per request
     )
 
-# Extract stars, text, and date for each review and save to Google Sheets
-if result:
-    for review in result:
-        stars, text, date = review['score'], review['content'], review['at']
+# Extract stars, text, and date for each review
+reviews_data = [(review['score'], review['content'], str(review['at'])) for review in result]
 
-        # Append a row to the sheet with stars, review text, and date
-        sheet.append_row([stars, text, str(date)])
+# Batch write to Google Sheets (reducing the number of API write requests)
+sheet.append_rows(reviews_data)
 
-    print(f"{len(result)} reviews saved to Google Sheets!")
-else:
-    print("No reviews found")
+print(f"{len(reviews_data)} reviews saved to Google Sheets!")
